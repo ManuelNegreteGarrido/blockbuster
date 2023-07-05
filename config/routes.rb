96edee7874e26624f7e9affdb40400501963f9a2
LeunamNegrete <manuelnegretegarrido@gmail.com>
2023-07-05 22:27:20 +0000

    blockbuster insertado index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :blockbuster
   resources :movies
   resources :clients
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "clients#index"
+  root "blockbuster#index"
 end
